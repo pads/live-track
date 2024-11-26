3292a889858c1114a2102c7f43bb0c06367c9235
Ben Paddock <pads@thisispads.me.uk>
2024-11-26 17:08:45 +0000

    Verify incoming message

--- inbound.ts
+++ inbound.ts
@@ -1,8 +1,35 @@
-import { Handler } from "aws-lambda";
+import { LambdaFunctionURLHandler } from "aws-lambda";
+import { verifySignature } from "@vonage/jwt";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 
-export const exec: Handler = async (event) => {
-  console.log("received event", event);
-  return {
-    statusCode: 204,
-  };
+export const exec: LambdaFunctionURLHandler = async (event) => {
+  const token: string | undefined = (event.headers.authorization || "").split(
+    " "
+  )[1];
+
+  const secretsManager = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+  const response = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "VONAGE_API_SIGNATURE_SECRET",
+    })
+  );
+  const secret = response.SecretString;
+
+  if (verifySignature(token, secret)) {
+    console.log("Request OK");
+    return {
+      statusCode: 204,
+    };
+  } else {
+    console.log("Request Invalid");
+    return {
+      statusCode: 401,
+      body: "Unauthorized",
+    };
+  }
 };
