954acd70b2761e6dfde945e83595d47795d41c8a
Ben Paddock <pads@users.noreply.github.com>
2024-11-27 18:30:07 +0000

    Setup s3 (#9)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: [".serverless/*"] },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
