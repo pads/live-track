1173f1e9b6e45e75ad9875e336f6c4dde615cf94
Ben Paddock <pads@thisispads.me.uk>
2024-11-27 17:06:57 +0000

    Push URL to S3

--- inbound.ts
+++ inbound.ts
@@ -5,22 +5,25 @@ import {
   SecretsManagerClient,
 } from "@aws-sdk/client-secrets-manager";
 import { parse } from "./parser";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 const secretsManager = new SecretsManagerClient({
   region: "us-east-1",
 });
+
+const s3 = new S3Client({ region: "us-east-1" });
 
 export const exec: LambdaFunctionURLHandler = async (event) => {
   const token: string | undefined = (event.headers.authorization || "").split(
     " ",
   )[1];
 
-  const response = await secretsManager.send(
+  const secretResponse = await secretsManager.send(
     new GetSecretValueCommand({
       SecretId: "VONAGE_API_SIGNATURE_SECRET",
     }),
   );
-  const secret = response.SecretString;
+  const secret = secretResponse.SecretString;
 
   if (!verifySignature(token, secret)) {
     console.log("Request unauthorized");
@@ -32,6 +35,15 @@ export const exec: LambdaFunctionURLHandler = async (event) => {
 
   const url = parse(event.body);
   console.log("Received URL", url);
+
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: "live-track-urls",
+      Key: "url",
+      Body: url,
+      ContentType: "text/plain",
+    }),
+  );
 
   return {
     statusCode: 204,
